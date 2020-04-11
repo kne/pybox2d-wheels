f5c568edfe68ca92daf623c4cbae5075dc6c8ac0
kne <sirkne@gmail.com>
2020-04-11 15:26:38 -0700

    more travis

--- config.sh
+++ config.sh
@@ -12,5 +12,5 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     python -c 'import sys; import Box2D; print("Imported Box2D successfully")'
-    # pytest -vv --pyargs Box2D.tests
+    pytest -vv --pyargs Box2D.tests
 }
