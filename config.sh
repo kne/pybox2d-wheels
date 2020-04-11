7515f299e37d01af79da46ac07bd9e7e55524d00
kne <sirkne@gmail.com>
2020-04-11 14:28:50 -0700

    Keep repo directory

--- config.sh
+++ config.sh
@@ -4,7 +4,9 @@
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
+    pushd
     build_swig
+    popd
 
     python setup.py build --force
 }
